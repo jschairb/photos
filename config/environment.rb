846492eea58f7f257ac0f4762625b734c7d840e7
Josh Schairbaum <joshua.schairbaum@gmail.com>
2009-04-06 07:05:22 -0400

    adding mms2r gem

--- config/environment.rb
+++ config/environment.rb
@@ -20,11 +20,10 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
 
-  config.gem 'thoughtbot-paperclip', 
-             :version => '2.2.7', :lib => 'paperclip', :source => "http://gems.github.com"
-  config.gem 'authlogic',
-             :version => '2.0.5', :source => "http://gems.rubyforge.org"
-
+  config.gem 'authlogic', :version => '2.0.5', :source => "http://gems.rubyforge.org"
+  config.gem 'mms2r', :version => "2.2.0", :source => "http://gems.rubyforge.org"
+  config.gem 'thoughtbot-paperclip', :version => '2.2.7', 
+                                     :lib => 'paperclip', :source => "http://gems.github.com"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
